f9491afd4be5194c73db603d25c88b1f5c595a7a
DcxIt <458593490@qq.com>
2017-08-02 00:46:33 +0800

    adasdasdasdasdasdasdasd

--- js/reception_controller.js
+++ js/reception_controller.js
@@ -1,7 +1,37 @@
 $(document).ready(
-
+	function(){
+		/*reception_banner_change();*/
+	}
 );
-
-function cp_reception_load(){
+var gPx = 48;
+/*function cp_reception_load(){
 	var ajaxUrl = "";
+}*/
+
+function reception_banner_change(){
+	var strLiLength = $(".ul2 li").length;
+	for(var i = 0;i<strLiLength+1;i++){
+		var display = $(".ul2 li").eq(i).css("display");
+		if(display == 'block'){
+			$(".ul2 li").eq(i).css("display","none");
+				var j = i + 1;
+				if(j == strLiLength){
+					var j = 0;
+				}
+				$(".ul2 li").eq(j).css("display","block");
+				break;
+		}
+	}
 }
+setInterval("reception_banner_change()",2000);
+
+function reception_paoma_light(){
+	gPx -= 1;
+	var maxPx = -1047;
+	if(gPx == maxPx){
+		gPx = 48;
+	}
+	var marginLeft = gPx+"px";
+	$(".container6-1-1 ul li").eq(0).css("margin-left",marginLeft);
+}
+setInterval("reception_paoma_light()",25);
