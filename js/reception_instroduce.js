dd6c084d3e5d2084a2ad8d3c75586e89a12cdf5e
DcxIt <458593490@qq.com>
2017-08-02 23:29:23 +0800

    dasdasdasd

--- js/reception_instroduce.js
+++ js/reception_instroduce.js
@@ -1,5 +1,6 @@
 
 /*公司简介内容切换*/
-function instroduce_html_change(){
-
+function instroduce_html_change(strFlag){
+	$(".instroduce_main_contents").css("display","none");
+	$("."+strFlag).css("display","block");
 }
